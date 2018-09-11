2c563f579d1e30aac3bd1b0788686040d6f94e46
Eduarda Passig <dudynha.passigs@gmail.com>
2018-09-11 09:21:00 -0300

    iniciando agenda

---
+++ Aula13/src/main/java/POO/Agenda.java
@@ -0,0 +1,18 @@
+package POO;
+
+public class Agenda {
+    private ArrayList<Pessoa> contatos;
+
+    public void addPessoa (Pessoa p){
+
+    }
+    public boolean removePessoa(String n,String s){
+        return true;
+    }
+    public boolean addTelefone(String r,String n,int plndex ){
+        return true;
+    }
+    public boolean addEmail(String r, String e,int plndex){
+        return true;
+    }
+}
